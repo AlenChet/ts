e8d43454a1020e2d46708a30d0c177c423aa855c
AlenaChet <chetvernina2016@gmail.com>
2024-02-20 16:38:32 +0300

    first commit

--- ci-template/src/js/__tests__/basic.test.js
+++ ci-template/src/js/__tests__/basic.test.js
@@ -1,7 +1,43 @@
-import sum from '../basic';
+import { Character, Team } from '../basic';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+describe('Team class', () => {
+    let team;
 
-  expect(result).toBe(6);
+    beforeEach(() => {
+        team = new Team();
+    });
+
+    test('Добавляем персонажа в команду', () => {
+        const character = new Character('Alena');
+        team.add(character);
+        expect(team.toArray()).toContain(character);
+    });
+
+    test('Ошибка, если есть персонаж', () => {
+        const character = new Character('Alena');
+        team.add(character);
+        expect(() => team.add(character)).toThrow();
+    });
+
+    test('Добавляем несколько персонажей', () => {
+        const character1 = new Character('Alena');
+        const character2 = new Character('Artem');
+        team.addAll(character1, character2);
+        expect(team.toArray()).toContain(character1);
+        expect(team.toArray()).toContain(character2);
+    });
+
+    test('Не добавляются дубликаты', () => {
+        const character1 = new Character('Alena');
+        const character2 = new Character('Artem');
+        team.addAll(character1, character2, character1);
+        expect(team.toArray()).toHaveLength(2);
+    });
+
+    test('Конвертируем в массив', () => {
+        const character1 = new Character('Alena');
+        const character2 = new Character('Artem');
+        team.addAll(character1, character2);
+        expect(team.toArray()).toEqual([character1, character2]);
+    });
 });
--- ci-template/src/js/basic.js
+++ ci-template/src/js/basic.js
@@ -1,7 +1,33 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+class Character {
+  constructor(name) {
+      this.name = name;
   }
-  return result;
 }
+
+class Team {
+  constructor() {
+      this.members = new Set();
+  }
+
+  add(player) {
+      if (this.members.has(player)) {
+          throw new Error(`Character ${player.name} is already in the team.`);
+      } else {
+          this.members.add(player);
+      }
+  }
+
+  addAll(...players) {
+      players.forEach(player => {
+          if (!this.members.has(player)) {
+              this.members.add(player);
+          }
+      });
+  }
+
+  toArray() {
+      return Array.from(this.members);
+  }
+}
+
+export { Character, Team };
