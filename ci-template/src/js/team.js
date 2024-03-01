9ee5f4a98c4a64b6a62e77d3c32c0266f0a4fb09
AlenaChet <chetvernina2016@gmail.com>
2024-03-01 13:37:19 +0300

    first commit

--- ci-template/src/js/team.js
+++ ci-template/src/js/team.js
@@ -1,27 +1,30 @@
 import Character from './character';
 
-export default class Team {
+class Team {
   constructor() {
     this.players = [];
+    this.numberOfPlayers = 0;
   }
 
   add(player) {
     if (player instanceof Character) {
       this.players.push(player);
+      this.numberOfPlayers += 1;
     } else {
       throw new Error('Invalid player object. Expected an instance of Character.');
     }
   }
 
-  [Symbol.iterator]() {
-    let current = 0;
-    const { players } = this;
-    return {
-      next() {
-        const done = current >= players.length;
-        const value = !done ? players[current++] : undefined;
-        return { value, done };
-      },
-    };
+  *[Symbol.iterator]() {
+    for (let player of this.players) {
+      yield player;
+    }
   }
 }
+
+const team = new Team();
+
+const player1 = new Character({ name: 'Лучник', type: 'Bowman', health: 50, level: 1, attack: 40, defence: 10 });
+const player2 = new Character({ name: 'Воин', type: 'Warrior', health: 60, level: 2, attack: 50, defence: 20 });
+team.add(player1);
+team.add(player2);
